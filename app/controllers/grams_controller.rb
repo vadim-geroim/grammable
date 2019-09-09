93bceb707a43267b97e57cba7217ebdc42ada1e3
vgeroim <vgeroim.qa@gmail.com>
2019-09-09 23:03:20 +0000

    add validation gram should be present

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -12,7 +12,7 @@ class GramsController < ApplicationController
     if @gram.valid?
       redirect_to root_path
     else
-      render :new, status: :unprocessabel_entity  
+      render :new, status: :unprocessable_entity  
     end
   end
 
