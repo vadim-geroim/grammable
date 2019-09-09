f8429b72a37909ec586ff8cff931d3d2aef5bedc
vgeroim <vgeroim.qa@gmail.com>
2019-09-09 22:12:28 +0000

    add Gram model to application

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,4 +1,24 @@
 class GramsController < ApplicationController
   def index
   end
+
+  def new
+    @gram = Gram.new
+  end
+
+  def create
+    @gram = Gram.create(gram_params)
+
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessabel_entity  
+    end
+  end
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
+  end
 end
